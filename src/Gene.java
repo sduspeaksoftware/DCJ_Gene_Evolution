192e893fc32f7a5aea9c62cb73938c81239d3aa1
rayiooo <rayiooo@foxmail.com>
2019-02-27 16:47:00 +0800

    Gene,Node类构造方法

--- src/Gene.java
+++ src/Gene.java
@@ -1,7 +1,18 @@
-
-
 public class Gene {
+	boolean reverse = false;  //正反
 	Node head;
 	Node tail;
 	String name;
+	public Gene(String name){
+		//判断正反
+		if(name.charAt(0) == '-'){
+			this.reverse = true;
+			this.name = name.subString(1);
+		}else{
+			this.name = name;
+		}
+		//头尾节点
+		this.head = new Node(name, "head");
+		this.tail = new Node(name, "tail");
+	}
 }
--- src/Node.java
+++ src/Node.java
@@ -2,5 +2,18 @@ public class Node {
 	Node sameLink;
 	Node diffLink;
 	String nameht;
-	
+	/*
+	 * @param name:基因名
+	 * @param type:头尾类型,head或tail
+	 */
+	public Node(String name, String type){
+		if(type.equals("head")){
+			this.nameht = name + "h";
+		}else if(type.equals("tail")){
+			this.nameht = name + "t";
+		}else{
+			assert(false);
+			System.out.println("Wrong Gene Node type!");
+		}
+	}
 }
