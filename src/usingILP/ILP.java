4add190e34e7aa7be0edf0cc4967f582f15e3a62
rayiooo <rayiooo@foxmail.com>
2019-03-22 15:27:19 +0800

    ILP等式及不等式构建完成。

--- src/usingILP/ILP.java
+++ src/usingILP/ILP.java
@@ -5,23 +5,41 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class ILP {
+	int k; //基因单链长
 	int xLength; //x之长度
-	ArrayList<ArrayList<Integer>> A;
-	ArrayList<Integer> b;
+	ArrayList<ArrayList<Integer>> A, Aeq;
+	ArrayList<Integer> b, beq;
 	
-	public ILP(int xLength) {
+	public ILP(int k) {
 		this.A = new ArrayList<ArrayList<Integer>>();
+		this.Aeq = new ArrayList<ArrayList<Integer>>();
 		this.b = new ArrayList<Integer>();
-		this.xLength = xLength;
+		this.beq = new ArrayList<Integer>();
+		this.k = k;
+		this.xLength = k * k + k * 2;
 	}
 	
 	/* 添加等式kx=b */
 	public void addEquation(ArrayList<Integer> k, int b) {
-		this.A.add(k);
-		this.b.add(b);
+		this.Aeq.add(k);
+		this.beq.add(b);
 	}
 	/* 使用HashMap 键-值 对应表添加等式kx=b */
 	public void addEquation(HashMap<Integer, Integer> k, int b) {
+		int[] k1 = new int[xLength];
+		Arrays.fill(k1, 0);
+		for(int i: k.keySet()) {
+			k1[i] = k.get(i);
+		}
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		for(int i: k1) {
+			array.add(i);
+		}
+		this.Aeq.add(array);
+		this.beq.add(b);
+	}
+	/* 添加不等式 */
+	public void addNoEquation(HashMap<Integer, Integer> k, int b) {
 		int[] k1 = new int[xLength];
 		Arrays.fill(k1, 0);
 		for(int i: k.keySet()) {
@@ -36,6 +54,26 @@ public class ILP {
 	}
 	
 	public void compute() {
+		//TODO
 		
+	}
+	
+	public int ijToE(int i, int j) {
+		//若用x(e)表示x(i,j),则 e = (i-1)*k + (j-1-k)
+		return (i-1)*xLength + (j-1-xLength);
+	}
+	public int yiToE(int i) {
+		//如每条链10个,则存在y1~y20,映射为x100~x119 (x(k^2 + i - 1))
+		return k*k + i - 1;
+	}
+	public int eToI(int e) {
+		//反求: i = (int) e/k + 1, j = e%k + k + 1
+		return (int)(e/xLength) + 1;
+	}
+	public int eToJ(int e) {
+		return e%xLength + xLength + 1;
+	}
+	public int eToYi(int e) {
+		return e - k*k + 1;
 	}
 }
--- src/usingILP/Solution.java
+++ src/usingILP/Solution.java
@@ -32,28 +32,70 @@ public class Solution {
 	}
 	
 	public static void ilp2(Genome g1,Genome g2) {
-		//12长Node,x标号i除以13整数部分表示g1中Node标号,i取余13表示g2中Node标号,类13进制,因此x标号i最大值为13^2-1
-		int genomeLength = g1.geneNodeList.size();
-		int system = genomeLength + 1; //进制
-		ILP ilp = new ILP((int) Math.pow(system, 2) - 1);
+		int k = g1.geneNodeList.size();
+		ILP ilp = new ILP(k); //前k^2位用于表示x(i,j),后2k位用于表示yi
 		//Ax=b构建
-		for(Node n: g1.geneNodeList) {
-			if(n.type.equals("head")) {
+		for(Node i: g1.geneNodeList) {
+			if(i.type.equals("head")) {
 				HashMap<Integer, Integer> equationLeft = new HashMap<Integer, Integer>();
 				int equationRight = 1;
-				for(Node i: n.diffLinks) {
+				for(Node j: i.diffLinks) {
 					//1.对位同名Node连接总数=1.仅考虑每个head结点,因head与tail同时连或不连.
-					equationLeft.put(n.number * system + (i.number - genomeLength), 1); //n连i的x(n,i)系数为1
+					equationLeft.put(ilp.ijToE(i.number, j.number), 1); //i连j的x(i,j)系数为1
 					//2.对位同名Node必须首尾同时连或同时不连( x(a,b) - x(a',b') == 0 )
 					HashMap<Integer, Integer> equationLeft2 = new HashMap<Integer, Integer>();
 					int equationRight2 = 0;
-					equationLeft2.put(n.number * system + (i.number - genomeLength), 1);
-					equationLeft2.put(n.samegene.number * system + (i.samegene.number - genomeLength), -1);
+					equationLeft2.put(ilp.ijToE(i.number, j.number), 1);
+					equationLeft2.put(ilp.ijToE(i.samegene.number, j.samegene.number), -1);
 					ilp.addEquation(equationLeft2, equationRight2);
 				}
 				ilp.addEquation(equationLeft, equationRight);
 			}
 		}
+		//Ax>=b构建
+		HashMap<Integer, Integer> noeq = new HashMap<Integer, Integer>();
+		for(Node i: g1.geneNodeList) {
+			for(Node j: g2.geneNodeList) {
+				//1.x(e)<=1, -x(e)<=0
+				noeq = new HashMap<Integer, Integer>();
+				noeq.put(ilp.ijToE(i.number, j.number), 1); //1*xe
+				ilp.addNoEquation(noeq, 1); //xe<=1
+				noeq = new HashMap<Integer, Integer>();
+				noeq.put(ilp.ijToE(i.number, j.number), -1); //-1*xe
+				ilp.addNoEquation(noeq, 0); //-xe<=0
+				//2.i*x(i,j)-yj+yi <= i
+				noeq = new HashMap<Integer, Integer>();
+				noeq.put(ilp.ijToE(i.number, j.number), i.number); //i*x(i,j)
+				noeq.put(ilp.yiToE(j.number), -1); //-yj
+				noeq.put(ilp.yiToE(i.number), 1); //+yi
+				ilp.addNoEquation(noeq, i.number);
+				//3.j*x(i,j)-yi+yj <= j
+				noeq = new HashMap<Integer, Integer>();
+				noeq.put(ilp.ijToE(i.number, j.number), j.number);
+				noeq.put(ilp.yiToE(i.number), -1);
+				noeq.put(ilp.yiToE(j.number), 1);
+				ilp.addNoEquation(noeq, j.number);
+			}
+		}
+		//4.0<=yi<=i
+		for(Node i: g1.geneNodeList) {
+			noeq = new HashMap<Integer, Integer>();
+			noeq.put(ilp.yiToE(i.number), -1);
+			ilp.addNoEquation(noeq, 0); //-yi<=0
+			noeq = new HashMap<Integer, Integer>();
+			noeq.put(ilp.yiToE(i.number), 1);
+			ilp.addNoEquation(noeq, i.number); //yi<=i
+		}
+		//5.0<=yj<=j
+		for(Node i: g2.geneNodeList) {
+			noeq = new HashMap<Integer, Integer>();
+			noeq.put(ilp.yiToE(i.number), -1);
+			ilp.addNoEquation(noeq, 0); //-yi<=0
+			noeq = new HashMap<Integer, Integer>();
+			noeq.put(ilp.yiToE(i.number), 1);
+			ilp.addNoEquation(noeq, i.number); //yi<=i
+		}
+		//送matlab计算
 		ilp.compute();
 	}
 }
